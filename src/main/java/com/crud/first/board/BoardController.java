2153783842096a6f91295d4bcb2431f6d9cd39ec
이혁 <master>
2024-04-23 17:45:41 +0900

    detatil commit

--- src/main/java/com/crud/first/board/BoardController.java
+++ src/main/java/com/crud/first/board/BoardController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +24,20 @@ public class BoardController {
     return "board/board.html";
   }
 
+  @GetMapping("/board/{id}")
+  public String selectBoard(Model model, @PathVariable Long id){
+    List<Board> result = boardRepository.findAll();
+    model.addAttribute("boards",result);
+
+    Optional<Board> idNum = boardRepository.findById(id);
+    if (idNum.isPresent()){
+      model.addAttribute("selectBoard",idNum);
+      return "board/selectBoard.html";
+    }
+    return "redirect:/board";
+  }
+
+
   @GetMapping("/create")
   public String create() {
     return "board/createBoard.html";
@@ -35,4 +51,6 @@ public class BoardController {
     boardRepository.save(board);
     return "redirect:/board";
   }
+
+
 }
