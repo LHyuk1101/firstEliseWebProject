510a00c2ffc316b145bde1c3e5eaac9108494761
이혁 <master>
2024-04-30 18:19:30 +0900

    SecurityConfig 수정

--- src/main/java/com/crud/first/SecurityConfig.java
+++ src/main/java/com/crud/first/SecurityConfig.java
@@ -24,18 +24,48 @@ public class SecurityConfig {
     return boardRepository.findAll().getFirst();
   }
 
+  //기존
+//  @Bean
+//  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//    http.csrf((csrf) -> csrf.disable());
+//
+//    http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/**").permitAll());
+//
+//    http.formLogin((formLogin) -> formLogin.loginPage("/login").defaultSuccessUrl("/board/"+getFirstBoardNum().getBoardId()));
+//
+//    http.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"));
+//
+//    return http.build();
+//  }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // CSRF 보호 비활성화
     http.csrf((csrf) -> csrf.disable());
+    // 요청 권한 설정
+    http.authorizeHttpRequests((authorize) -> {
+      authorize.requestMatchers("/","home.css","/dog.png").permitAll(); // "/" 경로는 모든 사용자에게 허용
+      authorize.anyRequest().authenticated(); // 그 외 모든 요청은 인증된 사용자만 접근 가능
+    });
 
-    http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/**").permitAll());
+    // 로그인 설정
+    http.formLogin((formLogin) ->
+            formLogin.loginPage("/login")
+                    .defaultSuccessUrl("/board/"+getFirstBoardNum().getBoardId())
+                    .permitAll()); // 로그인 페이지는 모든 사용자가 접근 가능해야 함
 
-    http.formLogin((formLogin) -> formLogin.loginPage("/login").defaultSuccessUrl("/board/"+getFirstBoardNum().getBoardId()));
+    // 로그아웃 설정
+    http.logout((logout) ->
+            logout.logoutUrl("/logout")
+                    .logoutSuccessUrl("/")); // 로그아웃 성공 시 "/" 로 리다이렉션
 
-    http.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"));
+    // 세션 관리 설정 (세션 만료 시 "/"로 리다이렉션)
+    http.sessionManagement((session) ->
+            session.invalidSessionUrl("/")); // 세션이 유효하지 않을 때 리다이렉트할 URL 설정
 
     return http.build();
   }
+
 
   @Bean
   public PasswordEncoder passwordEncoder() {
--- src/main/java/com/crud/first/home/HomeController.java
+++ src/main/java/com/crud/first/home/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@PreAuthorize("isAnonymous()")
 @Controller
 public class HomeController {
 
